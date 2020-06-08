d50c9ae50bdccfd7a84c67a3b51368686917b138
Ioane Lomidze <ioanelomidze@gmail.com>
2020-06-08 21:37:59 +0400

    dynamic data

--- public/quiz.js
+++ public/quiz.js
@@ -1,7 +1,26 @@
 new Vue({
     data: {
         name: 'კვერცხი',
-        items: [1,2,3,4]
+        items: [],
+        modal: false
     },
-    el: '#table',
+    mounted: function () {
+        this.getQuiz() // Calls the method before page loads
+    },
+    methods: {
+        getQuiz: function () {
+            fetch('quiz')
+                .then(res => res.json())
+                .then(data => this.items = data.quiz)
+        },
+        openModal: function () {
+            this.modal = true
+        },
+        closeModal: function(e) {
+            console.log(e.target.id)
+            if(e.target.id == 'notClose') return false;
+            this.modal = false;
+        }
+    },
+    el: '#question',
 })
