f76514b916d1bbf4f329fead9cb80599e6bc1473
Developers Imac <nikushascode@gmail.com>
2020-06-17 19:42:17 +0400

    markup finish without pagination

--- public/quiz.js
+++ public/quiz.js
@@ -1,26 +1,11 @@
 new Vue({
+    el: "#container",
     data: {
-        name: 'კვერცხი',
-        items: [],
-        modal: false
-    },
-    mounted: function () {
-        this.getQuiz() // Calls the method before page loads
+        activeClass: false
     },
     methods: {
-        getQuiz: function () {
-            fetch('quiz')
-                .then(res => res.json())
-                .then(data => this.items = data.quiz)
-        },
-        openModal: function () {
-            this.modal = true
-        },
-        closeModal: function(e) {
-            console.log(e.target.id)
-            if(e.target.id == 'notClose') return false;
-            this.modal = false;
+        toggleActive: function () {
+            this.activeClass = !this.activeClass;
         }
-    },
-    el: '#question',
+    }
 })
